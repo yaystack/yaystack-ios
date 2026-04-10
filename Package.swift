f77b3ff9634a0d3306ad986e9476000e2b13052e
Matthieu Narbot <m.narbot@gmail.com>
2026-04-10 17:58:56 +0200

    Release 1.0.0: dynamic framework with resources and Swift interfaces

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let version = "1.0.0"
-let checksum = "bfe5456a782ff01c01f1189608cdf65b26a704270e77c3f2cffb4a06421d4a2d"
+let checksum = "65a3855a1bbc0d0ffd5a91cdb7cc593b1454c1ae5c60229c5e7d6d871b13bd6c"
 
 let package = Package(
     name: "YaystackSDK",
