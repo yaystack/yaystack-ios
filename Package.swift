cd63e27cd82bfa4151e3814187a4b8f9dd994c7f
Matthieu Narbot <m.narbot@gmail.com>
2026-04-10 18:17:00 +0200

    Fix empty claims layout

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let version = "1.0.0"
-let checksum = "65a3855a1bbc0d0ffd5a91cdb7cc593b1454c1ae5c60229c5e7d6d871b13bd6c"
+let checksum = "720465897459673478f99234eaffe8992c94cb90313ad19d787e81c60add89da"
 
 let package = Package(
     name: "YaystackSDK",
